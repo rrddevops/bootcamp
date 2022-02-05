386e6fd9eaa1e139023abca23fc518e1af126cbd
Rodrigo Davila <rodrigordavila@gmail.com>
2022-02-05 00:59:02 -0300

    meu primeiro commit

---
+++ scripts/permissao.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /var/www/html
+chown www-data:www-data rds.conf.php
+chown www-data:www-data s3-conf.php
---
+++ scripts/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/www/html
+mv rds.conf.php ../
+mv s3-conf.php ../
+rm -R *
+mv ../rds.conf.php .
+mv ../s3-conf.php .
